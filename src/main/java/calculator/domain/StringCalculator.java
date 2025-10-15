bf2d903d65ff7db91d1b40c39dba1310d8de003e
sunJ0120 <sspure1214@gmail.com>
2025-10-15 21:29:19 +0900

    feat: 계산기 구현 완료

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,4 +1,17 @@
 package calculator.domain;
 
 public class StringCalculator {
+    private final String[] numbers; //받은 숫자들
+
+    public StringCalculator(String[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public int sum(){
+        int result = 0;
+        for(String number : numbers){
+            result += Integer.parseInt(number);
+        }
+        return result;
+    }
 }
