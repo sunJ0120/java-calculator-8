4c6b8e3b428045c3a85429e78cc03d692aafb683
sunJ0120 <sspure1214@gmail.com>
2025-10-15 21:31:55 +0900

    feat: 문자열 덧셈 계산기 v1 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.domain.Formula;
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Formula formula = new Formula(inputView.readFormula());
+        StringCalculator stringCalculator = new StringCalculator(formula.toNumbers());
+        outputView.printResult(stringCalculator.sum());
     }
 }
