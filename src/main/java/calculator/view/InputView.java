7e0ca3a6d42dc93793f91c9ad752a88040831673
sunJ0120 <sspure1214@gmail.com>
2025-10-16 18:55:01 +0900

    refactor: inputView 1차 리팩터링
    1. 매직 스트링 적용으로 상수화
    2. 자바 docs 공식 규약에 맞는 주석 설정
    3. 입력 검증을 inputView에 위임
    4. 외부에서 참조하지 않는 상수 변수와 메서드를 private 처리

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,10 +1,31 @@
+/**
+ * 클래스 이름: InputView
+ * <p>
+ * 버전 정보: 1.1
+ * <p>
+ * 날짜: 2025-10-16
+ * <p>
+ * 저작권 주의: Copyright (c) 2025 sspur
+ */
 package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String readFormula(){
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine(); //공식 가이드 라인에 따라 설정
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public String readFormula() {
+        System.out.println(INPUT_MESSAGE);
+        // 공식 가이드 라인에 따라 설정
+        String inputFormula = Console.readLine();
+        validateInputFormula(inputFormula);
+        return inputFormula;
+    }
+
+    // 입력 예외 처리
+    private void validateInputFormula(String inputFormula) {
+        if (inputFormula == null) {
+            throw new IllegalArgumentException("입력 형식이 잘못되었습니다.");
+        }
     }
 }
