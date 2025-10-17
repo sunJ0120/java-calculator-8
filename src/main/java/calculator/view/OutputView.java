0e024e31d1ea62b5f2e12f93a7aed6ea471040f8
sunJ0120 <sspure1214@gmail.com>
2025-10-17 23:00:54 +0900

    docs: OutputView.java에 Doc 주석 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,3 +1,12 @@
+/**
+ * 클래스 이름: OutputView
+ * <p>
+ * 버전 정보: 1.0
+ * <p>
+ * 날짜: 2025-10-17
+ * <p>
+ * 저작권 주의: Copyright (c) 2025 sspur
+ */
 package calculator.view;
 
 public class OutputView {
