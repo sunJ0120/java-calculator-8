d3abbac9b3436845a417ad9a1d9e015ee72887e8
sunJ0120 <sspure1214@gmail.com>
2025-10-17 22:56:50 +0900

    refactor: StringCalculator.java 1차 리팩터링
    1. null 체크 추가
    2. 숫자가 아닌 값이 들어올 경우의 Exception 처리 추가

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,16 +1,34 @@
+/**
+ * 클래스 이름: StringCalculator
+ * <p>
+ * 버전 정보: 1.1
+ * <p>
+ * 날짜: 2025-10-17
+ * <p>
+ * 저작권 주의: Copyright (c) 2025 sspur
+ */
 package calculator.domain;
 
+import calculator.domain.exception.InvalidNumberException;
+
 public class StringCalculator {
-    private final String[] numbers; //받은 숫자들
+    private final String[] numbers;    // 받은 숫자들
 
     public StringCalculator(String[] numbers) {
+        if (numbers == null) {    // null 안정성
+            throw new IllegalArgumentException("숫자 배열은 null일 수 없습니다");
+        }
         this.numbers = numbers;
     }
 
-    public int sum(){
+    public int sum() {
         int result = 0;
-        for(String number : numbers){
-            result += Integer.parseInt(number);
+        for (String number : numbers) {
+            try {
+                result += Integer.parseInt(number);
+            } catch (NumberFormatException e) {    // 숫자가 아닌 값이 들어올 경우
+                throw new InvalidNumberException(number, e);
+            }
         }
         return result;
     }
