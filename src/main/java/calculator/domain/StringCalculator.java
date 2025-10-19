ab23e64aaffd9ef0cb6f4e1e7db7143eefa41749
sunJ0120 <sspure1214@gmail.com>
2025-10-19 15:57:19 +0900

    fix: 음수 예외처리를 계산 로직에 위임

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,13 +1,14 @@
 package calculator.domain;
 
 import calculator.domain.exception.InvalidNumberException;
+import calculator.domain.exception.MinusNumberException;
 
 /**
  * 클래스 이름: StringCalculator
  * <p>
- * 버전 정보: 1.1
+ * 버전 정보: 1.2
  * <p>
- * 날짜: 2025-10-17
+ * 날짜: 2025-10-19
  */
 public class StringCalculator {
     private final String[] numbers;    // 받은 숫자들
@@ -19,14 +20,22 @@ public class StringCalculator {
         this.numbers = numbers;
     }
 
+    private static int validation(String number) {
+        try {
+            int num = Integer.parseInt(number);
+            if (num < 0) {    // 음수 검증
+                throw new MinusNumberException(number);
+            }
+            return num;
+        } catch (NumberFormatException e) {    // 숫자가 아닌 값이 들어올 경우
+            throw new InvalidNumberException(number, e);
+        }
+    }
+
     public int sum() {
         int result = 0;
         for (String number : numbers) {
-            try {
-                result += Integer.parseInt(number);
-            } catch (NumberFormatException e) {    // 숫자가 아닌 값이 들어올 경우
-                throw new InvalidNumberException(number, e);
-            }
+            result += validation(number);
         }
         return result;
     }
